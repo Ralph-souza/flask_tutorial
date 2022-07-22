b20efe3a64066462f365b4aa2025d5478ca82f13
Ralph <ralph_souza@hotmail.com>
2022-07-22 10:18:25 -0300

    Add application factory function and file to the project

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # flask_tutorial
-A simple user authentication page using flask.
+
+A simple blog application called Flaskr with user authentication page which will allow registration, log in,
+create posts, edit and delete their own posts. This project will use basically Flask in its structure, it is valid to 
+inform that this is a tutorial project to learn about this Python framework so, you may found inside its source code
+several commentaries explanning the steps inside functions and files.
+
+## How to run flask
+
+In order to run the project through bash terminal:
+> $ export FLASK_APP=flaskr
+> $ export FLASK_ENV=development
+> $ flask run
---
+++ flaskr/__init__.py
@@ -0,0 +1,40 @@
+# Application Factory -> Instance of Flask application
+import os
+
+from flask import Flask
+
+
+# Application Factory function
+def create_app(test_config=None):
+    # Create and configure the app
+    app = Flask(__name__, instance_relative_config=True)  # creates the Flask instance
+    # __name__ is the current Python module
+    # instance_relative_config=True tells the app that configurations files are relative to the instance folder
+    app.config.from_mapping(
+        # Used by Flask and extensions to keep data safe
+        SECRET_KEY='dev',
+        # The path where SQLIte database file will be saved
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # Load the instance config, if it exists, when not testing
+        # app.config.from_pyfile() overrides the default configuration with values taken from the config.py
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # Load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        # os.makedirs() ensures that app.instance_path exists
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, world!'
+
+    return app
